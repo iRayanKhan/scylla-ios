f1a69613fa09e6baa0979f89981ad9f72ab70f6d
Kevin Alavik <95900603+KevinAlavik@users.noreply.github.com>
2023-02-25 11:44:32 +0100

    Initial Commit

---
+++ scylla/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  scylla
+//
+//  Created by Kevin Alavik on 2023-02-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ scylla/scyllaApp.swift
@@ -0,0 +1,17 @@
+//
+//  scyllaApp.swift
+//  scylla
+//
+//  Created by Kevin Alavik on 2023-02-25.
+//
+
+import SwiftUI
+
+@main
+struct scyllaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
